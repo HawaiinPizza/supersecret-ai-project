196117bc189f14c6cc00a059469ff9f004333e72
MuazAlhaidar <omunam10@gmail.com>
2020-09-19 16:04:24 -0400

    Added functions to finde neighbors and filter them
    Functions Added:
    - function to get neighbors of current node
    - function to filter the list from the previous function to make sure they are not in the already explored set
    - fuction to make sure that the node we are trying to add to the neighbors is free to add and not a block

--- IterativeDeepeningSearch_IDS.cpp
+++ IterativeDeepeningSearch_IDS.cpp
@@ -5,39 +5,39 @@ void setMap() {
     // Set Entire Board
     for (short x = 0; x < 6; x++) {
         for (short y = 0; y < 5; y++) {
-            nodeIDSGrid[x][y].number = "  ";
-            nodeIDSGrid[x][y].isVisited = false;
-            nodeIDSGrid[x][y].status = grid_IDS::FRE;
-            nodeIDSGrid[x][y].position = std::make_pair(x, y);
+            nodeGrid[x][y].number = "  ";
+            nodeGrid[x][y].isVisited = false;
+            nodeGrid[x][y].status = grid_IDS::FRE;
+            nodeGrid[x][y].position = std::make_pair(x, y);
         }
     }
 
     // Set Block Location
     for (short z = 1; z < 5; z++) {
-        nodeIDSGrid[z][1].number = "##";
-        nodeIDSGrid[z][1].isVisited = false;
-        nodeIDSGrid[z][1].status = grid_IDS::BLOK;
+        nodeGrid[z][1].number = "##";
+        nodeGrid[z][1].isVisited = false;
+        nodeGrid[z][1].status = grid_IDS::BLOK;
     }
 
-    nodeIDSGrid[1][2].number = "##";
-    nodeIDSGrid[1][2].isVisited = false;
-    nodeIDSGrid[1][2].status = grid_IDS::BLOK;
+    nodeGrid[1][2].number = "##";
+    nodeGrid[1][2].isVisited = false;
+    nodeGrid[1][2].status = grid_IDS::BLOK;
 
-    nodeIDSGrid[3][2].number = "##";
-    nodeIDSGrid[3][2].isVisited = false;
-    nodeIDSGrid[3][2].status = grid_IDS::BLOK;
+    nodeGrid[3][2].number = "##";
+    nodeGrid[3][2].isVisited = false;
+    nodeGrid[3][2].status = grid_IDS::BLOK;
 
     // Set Start Location
-    nodeIDSGrid[3][0].number = "00";
-    nodeIDSGrid[3][0].isVisited = true;
-    nodeIDSGrid[3][0].status = grid_IDS::STRT;
+    nodeGrid[3][0].number = "00";
+    nodeGrid[3][0].isVisited = true;
+    nodeGrid[3][0].status = grid_IDS::STRT;
 
     // Set End Location
-    nodeIDSGrid[2][2].number = "  ";
-    nodeIDSGrid[2][2].isVisited = false;
-    nodeIDSGrid[2][2].status = grid_IDS::GOL;
+    nodeGrid[2][2].number = "  ";
+    nodeGrid[2][2].isVisited = false;
+    nodeGrid[2][2].status = grid_IDS::GOL;
 
-    exploredNodeNumber = 0;
+    exploredNumber = 0;
 }
 
 void iterativeDeepeningDFS() {
@@ -54,25 +54,70 @@ void depthFirstSearch(int depthLimit) {
     // Increment depth until it matches given depth limit
 }
 
-grid_IDS getNodeStatus(int x, int y) {
-    if (x < 0 || x >= MAP_WIDTH) {
-        return BLOK;
-    }
-
-    if (y < 0 || y >= MAP_HEIGHT) {
-        return BLOK;
-    }
-
-    return nodeIDSGrid[x][y].status;
-}
-
 void printMap() {
     std::cout << "------------------\n";
     for (short x = 0; x < 6; x++) {
         for (short y = 0; y < 5; y++) {
-            std::cout << nodeIDSGrid[x][y].number << "  ";
+            std::cout << nodeGrid[x][y].number << "  ";
         }
         std::cout << "\n";
     }
     std::cout << "------------------\n";
+}
+
+std::vector<node_IDS> getNodeNeighbors(std::pair<int, int> nodePosition) {
+
+    std::vector<node_IDS> neighbors;
+    grid_IDS nodeStatus;
+
+    nodeStatus = getNodeStatus(nodePosition.first, nodePosition.second - 1);
+    if (nodeStatus == FRE || nodeStatus == GOL) {
+        neighbors.push_back(
+            nodeGrid[nodePosition.first][nodePosition.second - 1]);
+    }
+
+    nodeStatus = getNodeStatus(nodePosition.first - 1, nodePosition.second);
+    if (nodeStatus == FRE || nodeStatus == GOL) {
+        neighbors.push_back(
+            nodeGrid[nodePosition.first - 1][nodePosition.second]);
+    }
+
+    nodeStatus = getNodeStatus(nodePosition.first, nodePosition.second + 1);
+    if (nodeStatus == FRE || nodeStatus == GOL) {
+        neighbors.push_back(
+            nodeGrid[nodePosition.first][nodePosition.second + 1]);
+    }
+
+    nodeStatus = getNodeStatus(nodePosition.first + 1, nodePosition.second);
+    if (nodeStatus == FRE || nodeStatus == GOL) {
+        neighbors.push_back(
+            nodeGrid[nodePosition.first + 1][nodePosition.second]);
+    }
+
+    return neighbors;
+}
+
+std::vector<node_IDS> checkExploredSet(std::vector<node_IDS> neighbors) {
+    std::vector<node_IDS> filteredNeighbors;
+
+    for (node_IDS temp : neighbors) {
+        std::unordered_map<std::string, node_IDS>::const_iterator got = exploredSet.find(temp.number);
+
+        if (got == exploredSet.end() && temp.status != BLOK)
+            filteredNeighbors.push_back(temp);
+    }
+
+    return filteredNeighbors;
+}
+
+grid_IDS getNodeStatus(int nodeRow, int nodeColumn) {
+    if (nodeRow < 0 || nodeRow >= MAP_WIDTH) {
+        return BLOK;
+    }
+
+    if (nodeColumn < 0 || nodeColumn >= MAP_HEIGHT) {
+        return BLOK;
+    }
+
+    return nodeGrid[nodeRow][nodeColumn].status;
 }
--- IterativeDeepeningSearch_IDS.hpp
+++ IterativeDeepeningSearch_IDS.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <unordered_map>
 #include <utility>
+#include <vector>
 
 const short MAP_WIDTH = 5;
 const short MAP_HEIGHT = 6;
@@ -24,20 +25,23 @@ struct node_IDS {
     grid_IDS status = grid_IDS::FRE;
 };
 
-node_IDS nodeIDSGrid[MAP_HEIGHT][MAP_WIDTH];
+node_IDS nodeGrid[MAP_HEIGHT][MAP_WIDTH];
 
 // Current number to increment and assign to the
 // number of a node_IDS
-int exploredNodeNumber;
+int exploredNumber;
 
-std::stack<node_IDS> frontierNodeSet;
-std::unordered_map<std::string, node_IDS> exploredNodeSet;
+std::stack<node_IDS> frontierSet;
+std::unordered_map<std::string, node_IDS> exploredSet;
 
 void setMap();
 void iterativeDeepeningDFS();
 void depthFirstSearch();
 void printMap();
 
-grid_IDS getNodeStatus(int x, int y);
+grid_IDS getNodeStatus(int nodeRow, int nodeColumn);
+
+std::vector<node_IDS> getNodeNeighbors(std::pair<int, int> nodePosition);
+std::vector<node_IDS> checkExploredSet(std::vector<node_IDS> neighbors);
 
 #endif
